365cc9c5f9e320447633b626a4a5661577e7fdec
ObviousThr33s <ObviousThr33s@users.noreply.github.com>
2024-08-14 19:03:10 -0700

    trusty screen control basics

--- main.rs
+++ main.rs
@@ -1,8 +1,22 @@
 pub mod utility;
 
-use utility::utils::utils as utils;
+use utility::utils::utils::{self as utils, Screen};
 
 fn main(){
- 	utils::foo();	
+	let s = Screen::new(30,30);
+	Screen::to_string(s);
+	print!("Screen initialized.");
 	
+	let mut i: u32 = 70;
+
+	let mut line: String = String::new();
+
+	while i < 90 {
+		i += 1;
+		utils::render(i);
+		
+		std::io::stdin().read_line(&mut line).unwrap();
+	}
+	
+
 }
