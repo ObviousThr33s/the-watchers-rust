b4d29a2dc8005f5f993d813d9c41e4590ed2c105
ObviousThr33s <ObviousThr33s@users.noreply.github.com>
2025-04-12 22:51:20 -0700

    more refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use the_watchers_rust::game;
-use the_watchers_rust::game::game::GameState;
-use the_watchers_rust::game::game::GameStates;
-use the_watchers_rust::logic;
-use the_watchers_rust::utils;
-use the_watchers_rust::utils::file_object::FileObject;
+
+use the_watchers_rust::game::core::{game_loop::GameLoop, game_state::GameState};
 
 pub fn main(){
-	let base_game_state:GameState = GameState::new();
-	let mut save_file:FileObject = FileObject::new();
+	let mut base_game_state:GameLoop = GameLoop::new();
+
+	//let mut save_file:FileObject = FileObject::new();
 
 	base_game_state.update();
 
